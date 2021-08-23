e2658b9187d8c42978298c3129a263376118272d
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-23 16:34:59 -0500

    updated album

---
+++ albums_seeder.sql
@@ -0,0 +1,40 @@
+USE codeup_test_db;
+
+SELECT * FROM albums;
+
+INSERT INTO albums  (artist, name, release_date, sales, genre)
+VALUES('Michael Jackson', 'Thriller', 1982, 47.9, 'pop, rock, funk'),
+      ('AC/DC', 'Back in Black', 1980, 29.6, 'hard rock'),
+      ('Meat Loaf', 'Bat Out of Hell', 1977, 21.7, 'hard rock, glam rock, progressive rock'),
+      ('Whitney Houston / Various Artists', 'The Bodyguard', 1992, 32.4, 'r&b, soul, pop, soundtrack'),
+      ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.4, 'progressive rock'),
+      ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'country rock, soft rock, folk rock'),
+      ('Bee Gees / Various Artists', 'Saturday Night Fever', 1977, 21.6, 'disco'),
+      ('Fleetwood Mac', 'Rumors', 1977, 27.9, 'soft rock'),
+      ('Shania Twain', 'Come On Over', 1997, 29.6, 'country, pop'),
+      ('Various artists', 'Grease: The Original Soundtrack from the Motion Picture', 1978, 14.4, 'soundtrack'),
+      ('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'hard rock, heavy metal, folk rock'),
+      ('Michael Jackson', 'Bad', 1987, 19.3, 'pop, funk, rock, blues'),
+      ('Alanis Morissette', 'Jagged Little Pill', 1995, 24.4, 'alternative rock'),
+      ('Michael Jackson', 'Dangerous', 1991, 16.3, 'r&b, funk, pop'),
+      ('Celine Dion', 'Falling into You', 1996, 20.2, 'pop, soft rock'),
+      ('Eagles', 'Hotel California', 1976, 21.5, 'soft rock'),
+      ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 13.1, 'rock'),
+      ('Various artists', 'Dirty Dancing', 1987, 17.9, 'pop, rock, r&b'),
+      ('Adele', '21', 2011, 25.3, 'pop, soul'),
+      ('Madonna', 'The Immaculate Collection', 1990, 19.4, 'pop, dance'),
+      ('Celine Dion', 'Let''s Talk About Love', 1997, 19.3, 'pop, soft rock'),
+      ('Metallica', 'Metallica', 1991, 21.2, 'heavy metal'),
+      ('The Beatles', '1', 2000, 22.6, 'rock'),
+      ('The Beatles', 'Abbey Road', 1969, 14.4, 'rock'),
+      ('ABBA', 'Gold: Greatest Hits', 1992, 23.0, 'pop, disco'),
+      ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 19.6, 'heartland rock'),
+      ('Pink Floyd', 'The Wall', 1979, 17.6, 'progressive rock'),
+      ('James Horner', 'Titanic: Music from the Motion Picture', 1997, 18.1, 'soundtrack'),
+      ('Dire Straits', 'Brothers in Arms', 1985, 17.7, 'roots rock, blues rock, soft rock'),
+      ('Nirvana', 'Nevermind', 1991, 16.7, 'grunge, alternative rock'),
+      ('Santana', 'Supernatural', 1999, 20.5, 'latin rock'),
+      ('Guns N'' Roses', 'Appetite for Destruction', 1987, 21.6, 'hard rock'),
+      ('Elton John', 'Goodebye Yellow Brick Road', 1973, 8.5, 'rock, pop rock, glam rock');
+
+
