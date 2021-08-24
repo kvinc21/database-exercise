96d6d4c520bdf12f9ba62278c3940cbb86c507bb
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-24 09:32:02 -0500

    update

---
+++ select_exercises.sql
@@ -0,0 +1,15 @@
+use codeup_test_db;
+
+select name
+from albums
+where artist = 'Pink Floyd';
+
+select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band was released';
+
+select genre from albums where name = 'Nevermind';
+
+select * from albums where release_date between 1990 and 2000;
+
+select * from albums where sales < 20;
+
+select * from albums where genre = 'Rock';
