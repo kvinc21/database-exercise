957ece74cba2486195f5c24610e6a7764a700a95
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-25 10:36:45 -0500

    update

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,13 +1,37 @@
 use employees;
 
-SELECT emp_no, first_name, last_name
+SELECT *
 FROM employees.employees e
-WHERE e.last_name IN ('Irena', 'Vidya', 'Maya');
+WHERE e.first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT count(*)
+from employees e
+where gender = 'M'
+and ( e.first_name = 'Irena'
+    or e.first_name = 'Vidya'
+    or e.first_name = 'Maya'
+    );
+
 
 select count(*)
 from employees.employees e
 where e.last_name like 'e%';
 
-select *
+select count(*)
 from employees.employees e
 where e.last_name like '%q%';
+
+select count(*)
+from employees.employees e
+where e.last_name like 'e%'
+or e.last_name like '%e';
+
+select count(*)
+from employees.employees e
+where e.last_name like 'e%'
+   and e.last_name like '%e';
+
+select count(*)
+from employees.employees e
+where e.last_name like '%q%'
+   and not e.last_name like '%qu%';
