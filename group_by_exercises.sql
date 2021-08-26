e2a0e6c1057455c126ca4b6d3c43ca8ca759d272
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-26 12:14:18 -0500

    update

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -3,3 +3,29 @@ use employees;
 select distinct title
 from titles;
 
+select last_name
+from employees
+where last_name like 'e%e'
+group by last_name;
+
+select last_name,
+       first_name
+from employees
+where last_name like 'e%e'
+group by first_name,
+         last_name;
+
+select last_name
+from employees
+where last_name like '%q%'
+and last_name not like '%qu%'
+group by last_name;
+
+ select  last_name, count(last_name)
+ as count_names
+ from employees                    
+ where last_name like '%q%'        
+ and last_name not like '%qu%'
+ group by last_name
+order by last_name desc;
+
