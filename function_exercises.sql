afa5769a84fdc7bd01855baa7a339e3a2d0929b2
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-26 11:47:52 -0500

    initial and updates

--- function_exercises.sql
+++ function_exercises.sql
@@ -1,2 +1,12 @@
 use employees;
 
+select concat(first_name, ' ', last_name) as full_name
+from employees
+where last_name like 'e%e';
+
+select upper(concat(first_name, ' ', last_name)) as full_name
+from employees
+where last_name like 'e%e';
+
+
+
---
+++ group_by_exercises.sql
@@ -0,0 +1,5 @@
+use employees;
+
+select distinct title
+from titles;
+
